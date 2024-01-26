b740fd2b8cdcc516118ba134122e088be1dd728d
brunoeduardodev <brunoeduardo.dev@gmail.com>
2024-01-26 13:50:30 -0300

    chore: reduce bundle size

--- src/cli/prompts.ts
+++ src/cli/prompts.ts
@@ -1,45 +1,30 @@
-import inquirer from "inquirer";
+import * as inquirer from "@inquirer/prompts";
 
 export const promptProjectName = async () => {
-  const { name } = await inquirer.prompt<{ name: string }>([
-    {
-      name: "name",
-      message: "What is the name of the project?",
-      type: "input",
-      transformer: (input: string) => {
-        return input.toLowerCase().replace(/\s+/g, "-");
-      },
+  const name = await inquirer.input({
+    message: "What is the name of the project?",
+    transformer: (input: string) => {
+      return input.toLowerCase().replace(/\s+/g, "-");
     },
-  ]);
+  });
 
   return name.toLowerCase().replace(/\s+/g, "-");
 };
 
 export const promptWithLint = async () => {
-  const { withLint } = await inquirer.prompt<{ withLint: boolean }>([
-    {
-      name: "withLint",
-      message: "Do you wish to include linting?",
-      type: "confirm",
-      default: true,
-    },
-  ]);
+  const withLint = await inquirer.confirm({
+    message: "Do you wish to include linting?",
+    default: true,
+  });
 
   return withLint;
 };
 
 export const promptPackageManager = async () => {
-  const { packageManager } = await inquirer.prompt<{
-    packageManager: "pnpm" | "yarn" | "npm";
-  }>([
-    {
-      name: "packageManager",
-      message: "Which package manager do you want to use?",
-      type: "list",
-      choices: ["pnpm", "yarn", "npm"],
-      default: "pnpm",
-    },
-  ]);
+  const packageManager = await inquirer.input({
+    message: "Which package manager do you want to use?",
+    default: "pnpm",
+  });
 
   return packageManager;
 };
